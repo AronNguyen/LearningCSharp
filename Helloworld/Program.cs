68843406b314db1f80666ee3a903c45380c68562
Aaron Nguyen <71195496+AronNguyen@users.noreply.github.com>
2021-11-16 21:06:55 -0600

    Add files via upload

---
+++ Helloworld/Program.cs
@@ -0,0 +1,25 @@
+﻿// To run this do dotnet run in the HelloWorld folder
+// Console.WriteLine == print()
+// Console.ReadLine() == input()
+// var variables can be equaled to anything like with python variables, does not need to defined a variable unless you want to
+// Environment.NewLine like new line in strings but since dotnet is combatitble with different Environment.NewLine uses that
+//  platforms newline command
+
+using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
